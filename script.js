20acb4f4cec011d2cd44f17a163aaab8acfb8f7b
Hk41878 <155512541+Hk41878@users.noreply.github.com>
2024-12-21 10:27:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,9 +13,82 @@ const episodes = {
     "Fire and Blood"
   ],
   S02: [
-    // Add Season 2 episode names here
+    "The North Remembers",
+    "The Night Lands",
+    "What Is Dead May Never Die",
+    "Garden of Bones",
+    "The Ghost of Harrenhal",
+    "The Old Gods and the New",
+    "A Man Without Honor",
+    "The Prince of Winterfell",
+    "Blackwater",
+    "Valar Morghulis"
   ],
-  // Add other seasons' episode names similarly
+  S03: [
+    "Valar Dohaeris",
+    "Dark Wings, Dark Words",
+    "Walk of Punishment",
+    "And Now His Watch Is Ended",
+    "Kissed by Fire",
+    "The Climb",
+    "The Bear and the Maiden Fair",
+    "Second Sons",
+    "The Rains of Castamere",
+    "Mhysa"
+  ],
+  S04: [
+    "Two Swords",
+    "The Lion and the Rose",
+    "Breaker of Chains",
+    "Oathkeeper",
+    "First of His Name",
+    "The Laws of Gods and Men",
+    "Mockingbird",
+    "The Mountain and the Viper",
+    "The Watchers on the Wall",
+    "The Children"
+  ],
+  S05: [
+    "The Wars to Come",
+    "The House of Black and White",
+    "High Sparrow",
+    "Sons of the Harpy",
+    "Kill the Boy",
+    "Unbowed, Unbent, Unbroken",
+    "The Gift",
+    "Hardhome",
+    "The Dance of Dragons",
+    "Mother's Mercy"
+  ],
+  S06: [
+    "The Red Woman",
+    "Home",
+    "Oathbreaker",
+    "Book of the Stranger",
+    "The Door",
+    "Blood of My Blood",
+    "The Broken Man",
+    "No One",
+    "Battle of the Bastards",
+    "The Winds of Winter"
+  ],
+  S07: [
+    "Dragonstone",
+    "Stormborn",
+    "The Queen's Justice",
+    "The Spoils of War",
+    "Eastwatch",
+    "Beyond the Wall",
+    "The Dragon and the Wolf"
+  ],
+  S08: [
+    "Winterfell",
+    "A Knight of the Seven Kingdoms",
+    "The Long Night",
+    "The Last of the Starks",
+    "The Bells",
+    "The Iron Throne"
+  ]
 };
 
 // Select Elements
@@ -36,18 +109,4 @@ function loadEpisodes(season) {
       card.innerHTML = `
         <img src="https://via.placeholder.com/200x120?text=Game+of+Thrones" alt="Episode Image">
         <h3>Episode ${index + 1}: ${title}</h3>
-        <a href="#" class="play-btn">Play</a>
-      `;
-
-      episodesDiv.appendChild(card);
-    });
-  }
-}
-
-// Event Listener
-seasonSelect.addEventListener("change", (e) => {
-  loadEpisodes(e.target.value);
-});
-
-// Initial Load
-loadEpisodes("S01");
+        <a href="#" class 
